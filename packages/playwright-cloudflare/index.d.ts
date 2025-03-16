b589161b63f320814e88e9f8ac8e10132dd61aa3
Rui Figueira <rui.figueira@gmail.com>
2025-03-16 10:36:10 +0000

    chore: remove sessionId from WorkersLaunchOptions

--- packages/playwright-cloudflare/index.d.ts
+++ packages/playwright-cloudflare/index.d.ts
@@ -10,8 +10,7 @@ export interface BrowserWorker {
 }
 
 export interface WorkersLaunchOptions {
-    keep_alive?: number; // milliseconds to keep browser alive even if it has no activity (from 10_000ms to 600_000ms, default is 60_000)
-    sessionId?: string;
+  keep_alive?: number; // milliseconds to keep browser alive even if it has no activity (from 10_000ms to 600_000ms, default is 60_000)
 }
 
 export function launch(endpoint: BrowserWorker, options?: WorkersLaunchOptions): Promise<Browser>;
