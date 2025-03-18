300b469538e2e43d06462bd89fb75454522e7a86
Rui Figueira <rui.figueira@gmail.com>
2025-03-18 16:05:22 +0000

    chore: add connect, sessions, history and limits functions

--- packages/playwright-cloudflare/src/index.ts
+++ packages/playwright-cloudflare/src/index.ts
@@ -3,22 +3,70 @@ import fs from 'fs';
 
 import { createInProcessPlaywright } from 'playwright-core/lib/inProcessFactory';
 
-import type { Browser, BrowserWorker, WorkersLaunchOptions } from '..';
+import type { ActiveSession, Browser, BrowserWorker, ClosedSession, HistoryResponse, LimitsResponse, SessionsResponse, WorkersLaunchOptions } from '..';
 import { storageManager } from './cloudflare/webSocketTransport';
 import { wrapClientApis } from './cloudflare/wrapClientApis';
+
+export { fs };
 
 const playwright = createInProcessPlaywright();
 wrapClientApis();
 
-const FAKE_HOST = 'ws://fake.host';
+const HTTP_FAKE_HOST = 'http://fake.host';
+const WS_FAKE_HOST = 'ws://fake.host';
+
+export async function connect(endpoint: BrowserWorker, sessionId: string): Promise<Browser> {
+  // keeps the endpoint and options for client -> server async communication
+  return await storageManager.run({ endpoint, options: { sessionId } }, 
+    async () => await playwright.chromium.connectOverCDP(WS_FAKE_HOST) as Browser
+  );
+}
 
 export async function launch(endpoint: BrowserWorker, options?: WorkersLaunchOptions): Promise<Browser> {
   // keeps the endpoint and options for client -> server async communication
   return await storageManager.run({ endpoint, options }, 
-    async () => await playwright.chromium.connectOverCDP(FAKE_HOST) as Browser
+    // TODO this actually connects, it doesn't launch. That means that closing the browser will not close the session
+    async () => await playwright.chromium.connectOverCDP(WS_FAKE_HOST) as Browser
   );
 }
 
-export { fs };
+export async function sessions(endpoint: BrowserWorker): Promise<ActiveSession[]> {
+  const res = await endpoint.fetch(`${HTTP_FAKE_HOST}/v1/sessions`);
+  const status = res.status;
+  const text = await res.text();
+  if (status !== 200) {
+    throw new Error(
+      `Unable to fetch new sessions: code: ${status}: message: ${text}`
+    );
+  }
+  const data: SessionsResponse = JSON.parse(text);
+  return data.sessions;
+}
+
+export async function history(endpoint: BrowserWorker): Promise<ClosedSession[]> {
+  const res = await endpoint.fetch(`${HTTP_FAKE_HOST}/v1/history`);
+  const status = res.status;
+  const text = await res.text();
+  if (status !== 200) {
+    throw new Error(
+      `Unable to fetch account history: code: ${status}: message: ${text}`
+    );
+  }
+  const data: HistoryResponse = JSON.parse(text);
+  return data.history;
+}
+
+export async function limits(endpoint: BrowserWorker): Promise<LimitsResponse> {
+  const res = await endpoint.fetch(`${HTTP_FAKE_HOST}/v1/limits`);
+  const status = res.status;
+  const text = await res.text();
+  if (status !== 200) {
+    throw new Error(
+      `Unable to fetch account limits: code: ${status}: message: ${text}`
+    );
+  }
+  const data: LimitsResponse = JSON.parse(text);
+  return data;
+}
 
 export default playwright;
