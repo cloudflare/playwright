4f3f09146c70ed9e86bea02b6da1c62fee8ea89a
Rui Figueira <rui.figueira@gmail.com>
2025-03-28 22:04:07 +0000

    refactor: make WebSocketTransport.sessionId readonly

--- packages/playwright-cloudflare/src/cloudflare/webSocketTransport.ts
+++ packages/playwright-cloudflare/src/cloudflare/webSocketTransport.ts
@@ -7,13 +7,13 @@ import { AsyncLocalStorage } from 'async_hooks';
 export const transportZone = new AsyncLocalStorage<WebSocketTransport>();
 
 export class WebSocketTransport implements ConnectionTransport {
-  ws: WebSocket;
-  pingInterval: NodeJS.Timer;
-  chunks: Uint8Array[] = [];
+  private _ws: WebSocket;
+  private _pingInterval: NodeJS.Timer;
+  private _chunks: Uint8Array[] = [];
   onmessage?: (message: ProtocolResponse) => void;
   onclose?: () => void;
-  sessionId: string;
-  
+  readonly sessionId: string;
+
   static async connect(): Promise<WebSocketTransport> {
     // read the endpoint and options injected in cliend side
     const transport = transportZone.getStore();
@@ -23,42 +23,42 @@ export class WebSocketTransport implements ConnectionTransport {
   }
 
   constructor(ws: WebSocket, sessionId: string) {
-    this.pingInterval = setInterval(() => {
-      return this.ws.send('ping');
+    this._pingInterval = setInterval(() => {
+      return this._ws.send('ping');
     }, 1000); // TODO more investigation
-    this.ws = ws;
+    this._ws = ws;
     this.sessionId = sessionId;
-    this.ws.addEventListener('message', event => {
-      this.chunks.push(new Uint8Array(event.data as ArrayBuffer));
-      const message = chunksToMessage(this.chunks, sessionId);
+    this._ws.addEventListener('message', event => {
+      this._chunks.push(new Uint8Array(event.data as ArrayBuffer));
+      const message = chunksToMessage(this._chunks, sessionId);
       if (message && this.onmessage)
         this.onmessage!(JSON.parse(message) as ProtocolResponse);
     });
-    this.ws.addEventListener('close', () => {
-      clearInterval(this.pingInterval as NodeJS.Timeout);
+    this._ws.addEventListener('close', () => {
+      clearInterval(this._pingInterval as NodeJS.Timeout);
       if (this.onclose)
         this.onclose();
     });
-    this.ws.addEventListener('error', e => {
+    this._ws.addEventListener('error', e => {
       // eslint-disable-next-line no-console
       console.error(`Websocket error: SessionID: ${sessionId}`, e);
-      clearInterval(this.pingInterval as NodeJS.Timeout);
+      clearInterval(this._pingInterval as NodeJS.Timeout);
     });
   }
 
   send(message: ProtocolRequest): void {
     for (const chunk of messageToChunks(JSON.stringify(message)))
-      this.ws.send(chunk);
+      this._ws.send(chunk);
   }
 
   close(): void {
-    clearInterval(this.pingInterval as NodeJS.Timeout);
-    this.ws.close();
+    clearInterval(this._pingInterval as NodeJS.Timeout);
+    this._ws.close();
     this.onclose?.();
   }
 
   async closeAndWait() {
-    if (this.ws.readyState === WebSocket.CLOSED)
+    if (this._ws.readyState === WebSocket.CLOSED)
       return;
     this.close();
     // TODO wait for close event
