83c2f0ca6492f4f921784501c549cffd77b79bff
Rui Figueira <rui.figueira@gmail.com>
2025-04-02 13:09:17 +0100

    chore: improve internal

--- packages/playwright-cloudflare/tests/src/workerFixtures.ts
+++ packages/playwright-cloudflare/tests/src/workerFixtures.ts
@@ -1,23 +1,8 @@
-/**
- * Copyright (c) Microsoft Corporation.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 import { _baseTest } from '@cloudflare/playwright/internal';
 import playwright from '@cloudflare/playwright';
 
-import type { Browser, BrowserType, BrowserContext, Page, Frame, PageScreenshotOptions, Locator, ViewportSize } from '@cloudflare/playwright/test';
-import type { BrowserContextOptions, ScreenshotMode, VideoMode } from '../../types/test';
+import type { ScreenshotMode, VideoMode } from '../../types/test';
+import type { BrowserContextOptions, Browser, BrowserType, BrowserContext, Page, Frame, PageScreenshotOptions, Locator, ViewportSize } from '@cloudflare/playwright/test';
 
 export { expect } from '@cloudflare/playwright/test';
 
@@ -210,7 +195,8 @@ export const test = platformTest.extend<PageTestFixtures & ServerFixtures & Test
 export async function rafraf(target: Page | Frame, count = 1) {
   for (let i = 0; i < count; i++) {
     await target.evaluate(async () => {
-      await new Promise(f => window.builtinRequestAnimationFrame(() => window.builtinRequestAnimationFrame(f)));
+      // @ts-ignore
+      await new Promise(f => window.builtins.requestAnimationFrame(() => window.builtins.requestAnimationFrame(f)));
     });
   }
 }
