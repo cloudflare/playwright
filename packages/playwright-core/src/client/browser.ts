1846ecbf8b759a57d739562e96140b14bea30f64
Kuba Orlik <jan.orlik@cloudflare.com>
2025-07-10 12:35:45 +0200

    Quick check if the smoke test was affected by the user agent

--- packages/playwright-core/src/client/browser.ts
+++ packages/playwright-core/src/client/browser.ts
@@ -57,7 +57,6 @@ export class Browser extends ChannelOwner<channels.BrowserChannel> implements ap
 
   async newContext(options: BrowserContextOptions = {}): Promise<BrowserContext> {
     if (!options.userAgent) {
-      options.userAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36";
     }
     return await this._innerNewContext(options, false);
   }
